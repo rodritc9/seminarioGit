74f3bcdbc7376a370110137b1015cef543f1b569
rodritc9 <torresc.rodri@gmail.com>
2021-05-24 21:45:51 -0300

    NUEVO ARCHIVO 4to

---
+++ nuevo.java
@@ -0,0 +1,10 @@
+package codigoFacil;
+
+public class codigoFacil {
+
+    public static void main(String[] args) {
+        System.out.println("AUltimo archivo"); // imprimir
+        
+        
+    }
+}
